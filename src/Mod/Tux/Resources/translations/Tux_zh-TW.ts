3e60f27e77c401a2fdab2960f1ef7ef0c42c9d96
DrBenson <Benson.Dr@GMail.com>
2024-10-18 08:17:33 +0800

    Update Tranditional Chinese translation.

--- src/Mod/Tux/Resources/translations/Tux_zh-TW.ts
+++ src/Mod/Tux/Resources/translations/Tux_zh-TW.ts
@@ -1,102 +1,115 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh-TW" sourcelanguage="en">
-  <context>
+<TS version="2.1" language="zh_TW" sourcelanguage="en_US">
+    <extra-po-header-language>zh_TW</extra-po-header-language>
+    <extra-po-header-language_team></extra-po-header-language_team>
+    <extra-po-header-last_translator>DrBenson &lt;Benson.Dr@GMail.com&gt;</extra-po-header-last_translator>
+    <extra-po-header-po_revision_date></extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date></extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version>FreeCAD Tux Language Translations v0.22_dev</extra-po-header-project_id_version>
+    <extra-po-header-x_generator>Poedit 3.2.2</extra-po-header-x_generator>
+    <extra-po-header_comment>#
+# Translators:
+# DrBenson &lt;Benson.Dr@GMail.com&gt;, 2023
+#
+    </extra-po-header_comment>
+    <extra-po-headers>Project-Id-Version,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,Language,MIME-Version,Content-Type,Content-Transfer-Encoding,Plural-Forms,X-Language,X-Source-Language,X-Qt-Contexts,X-Generator</extra-po-headers>
+<context>
     <name>NavigationIndicator</name>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="93"/>
-      <source>Select</source>
-      <translation>選擇</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="67"/>
+        <source>Select</source>
+        <translation>選擇</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="94"/>
-      <source>Zoom</source>
-      <translation>縮放</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="68"/>
+        <source>Zoom</source>
+        <translation>縮放</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="95"/>
-      <source>Rotate</source>
-      <translation>旋轉</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="69"/>
+        <source>Rotate</source>
+        <translation>旋轉</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="96"/>
-      <source>Pan</source>
-      <translation>平移</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="70"/>
+        <source>Pan</source>
+        <translation>平移</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="97"/>
-      <source>Tilt</source>
-      <translation>傾斜</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="71"/>
+        <source>Tilt</source>
+        <translation>傾斜</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="98"/>
-      <source>Navigation style</source>
-      <translation>導覽列樣式</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="72"/>
+        <source>Navigation style</source>
+        <translation>導覽列樣式</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="99"/>
-      <source>Page Up or Page Down key.</source>
-      <translation>Page Up 或 Page Down 按鍵。</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="73"/>
+        <source>Page Up or Page Down key.</source>
+        <translation>Page Up 或 Page Down 按鍵.</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="100"/>
-      <source>Rotation focus</source>
-      <translation>旋轉中心</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="74"/>
+        <source>Rotation focus</source>
+        <translation>旋轉中心</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="101"/>
-      <source>Middle mouse button or H key.</source>
-      <translation>滑鼠中鍵或 H 鍵</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="75"/>
+        <source>Middle mouse button or H key.</source>
+        <translation>滑鼠中鍵或 H 按鍵.</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="103"/>
-      <source>Middle mouse button.</source>
-      <translation>滑鼠中鍵。</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="77"/>
+        <source>Middle mouse button.</source>
+        <translation>滑鼠中鍵.</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="106"/>
-      <source>Navigation style not recognized.</source>
-      <translation>無法辨識導覽列樣式</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="80"/>
+        <source>Navigation style not recognized.</source>
+        <translation>無法辨識導覽列樣式.</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="577"/>
-      <source>Settings</source>
-      <translation>設定</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="331"/>
+        <source>Settings</source>
+        <translation>設定</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="578"/>
-      <source>Orbit style</source>
-      <translation>環繞模式</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="332"/>
+        <source>Orbit style</source>
+        <translation>環繞模式</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="579"/>
-      <source>Compact</source>
-      <translation>緊湊</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="333"/>
+        <source>Compact</source>
+        <translation>緊湊</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="580"/>
-      <source>Tooltip</source>
-      <translation>工具提示</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="334"/>
+        <source>Tooltip</source>
+        <translation>工具提示</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="581"/>
-      <source>Turntable</source>
-      <translation>旋轉台</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="335"/>
+        <source>Turntable</source>
+        <translation>可旋轉</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="582"/>
-      <source>Free Turntable</source>
-      <translation>自由旋轉台</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="336"/>
+        <source>Free Turntable</source>
+        <translation>自由旋轉</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="583"/>
-      <source>Trackball</source>
-      <translation>軌跡球</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="336"/>
+        <source>Trackball</source>
+        <translation>軌跡球</translation>
     </message>
     <message>
-      <location filename="../../NavigationIndicatorGui.py" line="584"/>
-      <source>Undefined</source>
-      <translation>未定義</translation>
+        <location filename="../../NavigationIndicatorGui.py" line="337"/>
+        <source>Undefined</source>
+        <translation>未定義</translation>
     </message>
-  </context>
+</context>
 </TS>
